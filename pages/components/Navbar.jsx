28fd542898ddba2571332d0115ec31012f98b2c9
Amir <amirhatam136@gmail.com>
2022-07-10 11:36:03 +0200

    create Header and navbar

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                    <div className="container">
+                        <a className="navbar-brand text-uppercase " >Artpoint</a>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { GetStaticProps } from "next";
 import Main from './components/Main'
+import Header from "./components/Header";
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -7,6 +9,7 @@ import styles from '../styles/Home.module.css'
 export default function Home(props) {
   return (
     <>
+      <Header data={props.data} />
       <Main data={props.data} />
     </>
   )
