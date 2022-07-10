abc4dfdac3ccb9b58af8718c899725ba07f13d42
Amir <amirhatam136@gmail.com>
2022-07-10 11:54:23 +0200

    add header content and footer

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Carrousel from './Carrousel';
+import styles from '../../styles/Header.module.css'
 
 
-export default function Header() {
+export default function Header(props) {
 
     return (
         <div >
             <Carrousel />
             <Navbar />
+            <div className={styles.headerTitle}>
+                <div className="text-light text-center">
+                    <h1 className='text-uppercase fw-light'>{props.data.title}</h1>
+                    <p className='fw-light h5 my-4'>{props.data.text}</p>
+                    <p>
+                        <a className="btn btn-lg btn-dark" href="https://artpoint.fr/" target='_blank'>Find Out More  &raquo;</a>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import { GetStaticProps } from "next";
 import Main from './components/Main'
 import Header from "./components/Header";
-
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Footer from "./components/Footer";
 
 export default function Home(props) {
   return (
     <>
       <Header data={props.data} />
       <Main data={props.data} />
+      <Footer />
     </>
   )
 }
